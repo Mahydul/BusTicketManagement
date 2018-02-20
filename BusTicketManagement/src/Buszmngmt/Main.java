e465ee53c46b4e5b5aaf5e79e9df526ed60e2325
Mahydul <mahydulislamshajal@gmail.com>
2018-02-20 17:45:12 +0600

    first commit

---
+++ BusTicketManagement/src/Buszmngmt/Main.java
@@ -0,0 +1,28 @@
+package Buszmngmt;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Mahydul Islam Shajal
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+           
+             Home_frame hf=new Home_frame();
+             hf.setVisible(true);
+
+    }
+    
+}
